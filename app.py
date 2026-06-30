889181b0eb9e3bae815cd2bb3261d91d9c1379b6
Zeitai <zeit.aionline@gmail.com>
2026-06-30 08:38:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -144,4 +144,5 @@ if __name__ == "__main__":
         level=logging.INFO,
         format="%(asctime)s %(levelname)s %(name)s %(message)s",
     )
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
